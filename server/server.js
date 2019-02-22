319940494292fae5ff1b9cbae9db8975698a21fe
Node.js Developer <atul1459@gmail.com>
2019-02-22 18:34:16 +0530

    SetUp App for Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo}=require('./models/todo');
 // var {User}=require('./models/user');
 
 var app=express();
+const port=process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -51,7 +52,7 @@ var id=req.params.id;
 });
 
 app.listen(3000,()=>{
-    console.log('Server Started');
+    console.log(`Server Started on port ${port}`);
 });
 
 module.exports={app};
